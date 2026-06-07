5c1c571a22505796bb85227bf2a7ad74fc3e3f25
Nicolas Marin <pnicolasmarin2007@gmail.com>
2026-06-07 14:47:34 -0500

    implements

--- main.cpp
+++ main.cpp
@@ -1,18 +1,100 @@
-#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+const int MAX = 1400005;
 
-// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+struct Rango {
+    long long valor;
+    long long inicio;
+    long long fin;
+};
 
-int main() {
-    // TIP Press <shortcut actionId="RenameElement"/> when your caret is at the <b>lang</b> variable name to see how CLion can help you rename it.
+Rango rangos[MAX];
+int total = 0;
 
-    const auto lang = "C++";
-    std::cout << "Hello and welcome to " << lang << "!\n";
+ifstream fin("entrada.txt");
+ofstream fout("salida.txt");
 
-    for (int i = 1; i <= 5; i++) {
-        // TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        std::cout << "i = " << i << std::endl;
+int buscarPosicion(long long pos) {
+    int izq = 0, der = total - 1;
+
+    while (izq <= der) {
+        int mid = izq + (der - izq) / 2;
+
+        if (rangos[mid].inicio <= pos && pos <= rangos[mid].fin) {
+            return mid;
+        }
+        else if (rangos[mid].fin < pos) {
+            izq = mid + 1;
+        }
+        else {
+            der = mid - 1;
+        }
     }
 
+    return -1;
+}
+
+int buscarPrimero(long long pos) {
+    int izq = 0, der = total - 1, resultado = total;
+
+    while (izq <= der) {
+        int mid = izq + (der - izq) / 2;
+
+        if (rangos[mid].inicio >= pos) {
+            resultado = mid;
+            der = mid - 1;
+        }
+        else {
+            izq = mid + 1;
+        }
+    }
+
+    return resultado;
+}
+
+int main() {
+
+    int N;
+    fin >> N;
+
+    for (int i = 0; i < N; i++) {
+        fin >> rangos[i].valor >> rangos[i].inicio >> rangos[i].fin;
+    }
+    total = N;
+    
+    int Q;
+    fin >> Q;
+
+    char operacion[20];
+    for (int i = 0; i < Q; i++) {
+
+        fin >> operacion;
+
+        if (strcmp(operacion, "VALUE") == 0) {
+
+        }
+        else if (strcmp(operacion, "SUM") == 0) {
+
+        }
+        else if (strcmp(operacion, "UPDATE") == 0) {
+        }
+        else if (strcmp(operacion, "FREQUENCY") == 0) {
+        }
+        else if (strcmp(operacion, "MAX_RANGE") == 0) {
+        }
+        else if (strcmp(operacion, "MIN_RANGE") == 0) {
+
+        }
+        else if (strcmp(operacion, "DECOMPRESS") == 0) {
+
+        }
+        else if (strcmp(operacion, "COUNT_RANGES") == 0) {
+        }
+        else if (strcmp(operacion, "MERGE") == 0) {
+        }
+    }
+
+    fin.close();
+    fout.close();
     return 0;
-    // TIP See CLion help at <a href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>. Also, you can try interactive lessons for CLion by selecting 'Help | Learn IDE Features' from the main menu.
 }
